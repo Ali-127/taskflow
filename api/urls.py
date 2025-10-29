177247b026684cd83dd86fd001ec0b21174318e2
Ali Mehdizadeh <alimehdizadeh127@gmail.com>
2025-10-29 11:27:31 +0330

    Feat: Add API document endpoints.

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from .views import ProjectViewSet, RegisterUserView, TaskViewSet
 
 
@@ -9,9 +10,17 @@ router.register(r'projects', ProjectViewSet, basename='project')
 router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
+    # Authentication endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register', RegisterUserView.as_view() , name='register'),
-    path('api/', include(router.urls))
+    
+    # API endpoints
+    path('api/', include(router.urls)),
+    
+    # API Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
